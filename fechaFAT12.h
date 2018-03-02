50acbd3b6ae24d63f884affeadb2aaedd2b1e05c
JoseLuisA <36089551+JoseLuisA@users.noreply.github.com>
2018-03-02 17:32:04 -0600

    Add files via upload

--- fechaFAT12.h
+++ fechaFAT12.h
@@ -2,12 +2,23 @@
 #ifndef FEHCA_FAT12
 #define FECHA_FAT12
 
-typedef
+
 struct fechafat12{
     unsigned dia:5;
     unsigned mes:4;
     unsigned anio:7;
-}FechaFat12;
+} __attribute__((packed));//FechaFat12;
+
+typedef struct fechafat12 FechaFAT12;
+
+struct Int16{
+    unsigned myInt16:16;
+    }__attribute__((packed));
+
+typedef struct Int16 uInt16;
+
+union UInt16{
+};
 
 
-#endif // FEHCA_FAT12
+#endif // FECHA_FAT12
